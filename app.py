e476fd0265d707d41e78d2125630ca332b53f73c
Ahmed Lotfy <lotfyah@yahoo.com>
2020-10-20 11:08:44 -0500

    Update app.py

--- app.py
+++ app.py
@@ -135,4 +135,4 @@ if __name__ == "__main__":
 	print(("* Loading Keras model and Flask starting server..."
 		"please wait until server has fully started"))
 	load_model()
-	app.run(debug=True)
+	app.run()
